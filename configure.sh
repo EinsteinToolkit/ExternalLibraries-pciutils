424e06bb1f530ea7e6f22361eaa9b2e603521c37
eschnett <eschnett@12efe93d-9b5d-4c7d-a57e-20a727e60333>
2016-06-29 22:07:56 +0000

    Correct and simplify setting LDFLAGS
    git-svn-id: http://svn.cactuscode.org/projects/ExternalLibraries/pciutils/trunk@31 12efe93d-9b5d-4c7d-a57e-20a727e60333

--- configure.sh
+++ configure.sh
@@ -58,7 +58,8 @@ PCIUTILS_DIR=${INSTALL_DIR}
           exit 1
         fi
 
-        export LDFLAGS
+        export LDFLAGS="${CFLAGS} ${LDFLAGS}"
+        export LDLIBS="-lz"
         unset LIBS
         if echo '' ${ARFLAGS} | grep 64 > /dev/null 2>&1; then
             export OBJECT_MODE=64
@@ -72,29 +73,13 @@ PCIUTILS_DIR=${INSTALL_DIR}
         echo "pciutils: Unpacking archive..."
         pushd ${BUILD_DIR}
         ${TAR?} xzf ${SRCDIR}/dist/${NAME}.tar.gz
-        pushd ${NAME}
-        ${PATCH?} -p1 < ${SRCDIR}/dist/remove-gcc.patch
-        # Some (ancient but still used) versions of patch don't support the
-        # patch format used here but also don't report an error using the
-        # exit code. So we use this patch to test for this
-        ${PATCH?} -p1 < ${SRCDIR}/dist/patchtest.patch
-        if [ ! -e .patch_tmp ]; then
-            echo 'BEGIN ERROR'
-            echo 'The version of patch is too old to understand this patch format.'
-            echo 'Please set the PATCH environment variable to a more recent '
-            echo 'version of the patch command.'
-            echo 'END ERROR'
-            exit 1
-        fi
-        rm -f .patch_tmp
-	popd
         
         echo "pciutils: Configuring..."
         cd ${NAME}
         # pciutils does not have a configure script; doing nothing
         
         echo "pciutils: Building..."
-        ${MAKE} PREFIX=${PCIUTILS_DIR} ZLIB=yes DNS=no SHARED=no
+        ${MAKE} PREFIX="${PCIUTILS_DIR}" CC="${CC}" CFLAGS="${CFLAGS}" OPT= LDFLAGS="${LDFLAGS}" LDLIBS="${LDLIBS}" AR="${AR}" RANLIB="${RANLIB}" ZLIB=yes DNS=no SHARED=no
         
         echo "pciutils: Installing..."
         ${MAKE} PREFIX=${PCIUTILS_DIR} install install-lib
